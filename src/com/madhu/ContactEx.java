66e02b28858645511155b1ccf4198582bcd15856
Madhusudhan <MADHUSUDHAN@RAMARAJU>
2016-02-03 21:20:30 +0000

    Second commit...

--- src/com/madhu/ContactEx.java
+++ src/com/madhu/ContactEx.java
@@ -7,7 +7,7 @@ public class ContactEx {
 		
 		Contact c1= new Contact();
 		c1.setId(111);
-		c1.setName("Madhusudhan");
+		c1.setName("My Name");
 		c1.setAddress("ABCD Street,XYZ City,UK");
 		System.out.println(c1.getId());
 		System.out.println(c1.getName());

1582434c225ba8578e706033584376693430ea2a
NMS <NMS@MADHUSUDHAN.lan>
2016-02-03 20:05:05 +0000

    Changes Made

---
+++ src/com/madhu/Contact.java
@@ -0,0 +1,26 @@
+package com.madhu;
+
+public class Contact {
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	private String name;
+	private String address;
+
+}
